ef3c0c4bbf7b92174ff4d34a3df809157d2c7811
Gabriel Grijincu <gabriel@grijincu.ro>
2020-12-02 17:24:37 +0200

    Initial state

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+declare module 'meteor/meteor' {
+  interface CompositeHandler<T = any> {
+    collectionName?: string;
+    children?: CompositeHandler<T>[];
+    find(): Mongo.Cursor<T>;
+  }
+
+  module Meteor {
+    function publishComposite(name: string, config: CompositeHandler<any> | CompositeHandler<any>[]): void;
+
+    function publishComposite(
+      name: string,
+      configFunc: (this: Subscription, ...args: any[]) => CompositeHandler<any> | CompositeHandler<any>[]
+    ): void;
+  }
+}
